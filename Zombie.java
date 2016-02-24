e6d0765154a10c5c5419e12141650b84183ccf13
jamperr <jp55674@unm.edu>
2016-02-24 00:26:28 -0700

    zombies now have random heading when they spawn

--- Zombie.java
+++ Zombie.java
@@ -1,6 +1,7 @@
 //James Perry
 //2-18-2016
 import java.util.ArrayList;
+import java.util.Random;
 
 
 
@@ -11,10 +12,38 @@ public class Zombie
   private int headingRow;
   private int headingCol;
   
+  public void move()
+  {
+    
+  }
+  
   public Zombie(int startRow,int startCol)
   {
     this.currentRow = startRow;
     this.currentCol = startCol;
+    Random rand = new Random();
+    int heading = rand.nextInt(4);
+    switch(heading)
+    {
+      case 0:
+      headingRow = 0;
+      headingCol = 1;
+      break;
+      case 1:
+      headingRow = 1;
+      headingCol = 0;
+      break;
+      case 2:
+      headingRow = -1;
+      headingCol = 0;
+      break;
+      case 3:
+      headingRow = 0;
+      headingCol = -1;
+      break;
+    
+    }
+    
   }
   
   public void pathFinding(int startRow,int startCol,int goalRow, int goalCol)
