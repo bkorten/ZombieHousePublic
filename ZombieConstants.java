cf25a636a6e2ce961e0fc82d0935122cee01c12a
jamperr <jp55674@unm.edu>
2016-02-24 00:27:13 -0700

    added new constants

--- ZombieConstants.java
+++ ZombieConstants.java
@@ -8,4 +8,6 @@ public class ZombieConstants
   public static final int WALL = 4;
   public static final int PLAYER = 8;
   public static final int EXIT = 5;
+  public static final int MASTER_ZOMBIE = 7;
+  public static final int ZOMBIE = 9;
 }
