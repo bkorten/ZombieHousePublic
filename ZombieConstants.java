d4690ac513fb7fb1168f78ad2b6070ba9d8b343f
jamperr <jp55674@unm.edu>
2016-02-27 13:34:32 -0700

    added a new constant

--- ZombieConstants.java
+++ ZombieConstants.java
@@ -5,10 +5,13 @@ public class ZombieConstants
   public static final int FLOOR = 0;
   public static final int HALLWAY = 6;
   public static final int OBSTACLE = 1;
+  public static final int DOORWAY = 3;
   public static final int ROOM = 2;
   public static final int WALL = 4;
   public static final int PLAYER = 8;
   public static final int EXIT = 5;
   public static final int MASTER_ZOMBIE = 7;
   public static final int ZOMBIE = 9;
+  public static final int NUM_ROWS = 35;
+  public static final int NUM_COLS = 75;
 }
