ef4537a5e1f4baf7abd392d2f35a312ae3bc17da
jamperr <jp55674@unm.edu>
2016-03-06 14:02:04 -0700

    added graphicsID

--- LineZombie.java
+++ LineZombie.java
@@ -2,13 +2,9 @@
 //2-18-2016
 public class LineZombie extends Zombie
 {
-  public void makeDecision()
+  public LineZombie(int startRow, int startCol, int graphicsID)
   {
-    
-  }
-  
-  public LineZombie(int startRow, int startCol)
-  {
-    super(startRow,startCol);
+    super(startRow,startCol,graphicsID);
   }
 }
+
