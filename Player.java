aba3e80bdea5e8b21cf9ff2e3d2148d58ff19283
bkorten <bkorten@unm.edu>
2016-03-05 20:25:31 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,43 +1,54 @@
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.PointLight;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Cylinder;
 import javafx.scene.transform.Rotate;
 
 
 public class Player 
 {
+  Cylinder boundingCylinder;	
   PerspectiveCamera playerCamera;
   PointLight playerLight;
   Color lightColor= new Color(0.01,0.01,0.01,.01);
   Rotate xRotate,yRotate;
   
-  double playerStartZ,playerStartX,playerCurZ,playerCurX;
+  
+  int boundingRadius = 50;
+  int boundingHeight = 250;
+  
+  double pivotX, pivotZ;
+  double playerStartZ,playerStartX,playerOldX,playerOldZ,playerCurZ,playerCurX;
   double nearClip = 0.1;
   double farClip = 1000;
   double feildOfView = 45;
   final double playerY=-300;
   
-  public Player(double playerStartZ,double playerStartX)
+  public Player(double playerStartX,double playerStartZ)
   {
-	  this.xRotate=new Rotate(0,0,0,0,Rotate.X_AXIS);
-	  this.yRotate=new Rotate(0,0,0,0,Rotate.Y_AXIS);
+	  
+	  
+	  
 	  
 	  this.playerStartZ=playerStartZ;
 	  this.playerStartX=playerStartX;
 	  this.playerCurX=playerStartX;
 	  this.playerCurZ=playerStartZ;
+	  yRotate= new Rotate(0,0,0,0,Rotate.Y_AXIS);
+	  
+	  
 	  
 	  //camera init
 	  this.playerCamera= new PerspectiveCamera(false);
-	  this.playerCamera.getTransforms().add(xRotate);
-	  this.playerCamera.getTransforms().add(yRotate);
+	  
 	  this.playerCamera.setTranslateX(playerStartX);
 	  this.playerCamera.setTranslateZ(playerStartZ);
 	  this.playerCamera.setTranslateY(playerY);
+	
 	  this.playerCamera.setNearClip(nearClip);
 	  this.playerCamera.setFarClip(farClip);
       this.playerCamera.setFieldOfView(feildOfView);
-	  this.playerCamera.setRotationAxis(Rotate.Y_AXIS);
+	  this.playerCamera.getTransforms().add(yRotate);
 	  
 	  //light init
 	  this.playerLight= new PointLight();
@@ -45,19 +56,52 @@ public class Player
 	  this.playerLight.setTranslateX(playerStartX);
 	  this.playerLight.setTranslateZ(playerStartZ);
 	  this.playerLight.setTranslateY(playerY);
-	  this.playerLight.getTransforms().addAll(xRotate,yRotate);
+	  this.playerLight.getTransforms().add(yRotate);
+	  
+	  //bounding cylinder
+	  this.boundingCylinder=new Cylinder(boundingRadius,boundingHeight);
+	  this.boundingCylinder.setTranslateX(playerStartX);
+	  this.boundingCylinder.setTranslateY(playerY);
+	  this.boundingCylinder.setTranslateZ(playerStartZ);
+	  
+	  //setPivots();
+  }
+  
+  public void rotatePlayer(double rotateAngle)
+  {
+	  
+	  playerCamera.setRotationAxis(Rotate.Y_AXIS);
+	  playerCamera.setRotate(rotateAngle);
+  }
+  
+  public void setPivots()
+  {
+	 
+	  
+	  this.yRotate.setPivotX(this.playerCurX);
+	  this.yRotate.setPivotZ(this.playerCurZ);
   }
   
   public void translatePlayer(double transX, double transZ)
   {
-	 this.playerCurX += transX;
-	 this.playerCurZ += transZ;
+	 this.playerOldX =this.playerCurX;
+	 this.playerOldZ =this.playerCurZ;
+	 
+	 this.playerCurX = transX;
+	 this.playerCurZ = transZ;
+	 
 	 
 	 this.playerCamera.setTranslateX(playerCurX);
 	 this.playerCamera.setTranslateZ(playerCurZ);
 	 
+	 
 	 this.playerLight.setTranslateX(playerCurX);
 	 this.playerLight.setTranslateZ(playerCurZ);
+	 
+	 this.boundingCylinder.setTranslateX(playerCurX);
+	 this.boundingCylinder.setTranslateZ(playerCurZ);
+	 
+	 
   }
   
 }
