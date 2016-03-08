6f3924008cdc155504937f63bc8e9b1b82e8286c
jamperr <jp55674@unm.edu>
2016-03-08 13:02:05 -0700

    fixed comments

--- Tile.java
+++ Tile.java
@@ -1,3 +1,7 @@
+/**
+ * This class is used by Graph.java in order to make A* pathfinding easier.
+ * A "tile" is a bird's eye view square of the map.
+ */
 public class Tile implements Comparable<Tile>
 {
   private int row;
@@ -5,20 +9,27 @@ public class Tile implements Comparable<Tile>
   private int col;
   public int getCol(){return this.col;}
   
+  //used to retrace path
   private Tile previous;
   public Tile getPrevious(){return this.previous;}
   public void setPrevious(Tile x){this.previous = x;}
+  
+  //floor, wall,player etc
   private int type;
   public int getType(){return this.type;}
   public void setType(int x){this.type = x;}
   
+  //f=g+h
   private double f_value;
   public double getFValue(){return this.f_value;}
   public void setFValue(double x){this.f_value = x;}
+  
+  //cost so far
   private double g_value;
   public double getGValue(){return this.g_value;}
   public void setGValue(double x){this.g_value = x;}
   
+  //heuristic to goal
   private double h_value;
   public double getHValue(){return this.h_value;}
   public void setHValue(double x){this.h_value = x;}
@@ -27,6 +38,7 @@ public class Tile implements Comparable<Tile>
   public void setPathMarker(int x){this.pathMarker = x;}
   public int getPathMarker(){return this.pathMarker;}
 
+  //used by a pathFinding
   public boolean equalsTile(Tile other)
   {
     if(this.col != other.getCol()) return false;
@@ -34,13 +46,14 @@ public class Tile implements Comparable<Tile>
     return true;
   }
   
-  
   public Tile(int row, int col, int type)
   {
     this.row = row;
     this.col = col;
     this.type = type;
   }
+  
+  //used by a PriorityQueue in Graph.java
   @Override
   public int compareTo(Tile o) 
   {
@@ -49,6 +62,4 @@ public class Tile implements Comparable<Tile>
     else if(this.getFValue() == o.getFValue()) return 0;
     return 0;
   }
-  
 }
-
