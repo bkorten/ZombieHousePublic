e3446d7d11fe461e1b20f304b505dff0bde2b0db
jamperr <jp55674@unm.edu>
2016-03-08 13:04:07 -0700

    fixed formatting

--- RandomZombie.java
+++ RandomZombie.java
@@ -1,14 +1,9 @@
-import java.util.Random;
-
 //James Perry
 //2-18-2016
 public class RandomZombie extends Zombie 
 {
-  
   public RandomZombie(int startRow, int startCol, int graphicsID) 
   {
     super(startRow, startCol,graphicsID);  
   }
- 
-  
 }
