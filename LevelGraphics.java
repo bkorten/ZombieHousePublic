63d4f7115eb204dc73cff2ae3aa5b8a15fcfa1be
bkorten <bkorten@unm.edu>
2016-03-07 19:40:20 -0700

    Update LevelGraphics.java

--- LevelGraphics.java
+++ LevelGraphics.java
@@ -10,7 +10,7 @@ public class LevelGraphics extends Group
 {
   int map[][];
   int mapWidth,mapHeight,mapCenterX,mapCenterY,level;
-  Door exit;
+  int tilecount=1;
   
   double playerStartX,playerStartZ;
   double masterZombieX, masterZombieZ;
@@ -20,8 +20,13 @@ public class LevelGraphics extends Group
   ArrayList<Floor> floor= new ArrayList<Floor>();
   ArrayList<Floor> ceiling= new ArrayList<Floor>();
   ArrayList<Obstacle> obstacles= new ArrayList<Obstacle>();
+  ArrayList<GraphicsFireTrap> fireTraps= new ArrayList<GraphicsFireTrap>();
+  ArrayList<Door> exit =new ArrayList<Door>(2);
   ArrayList<Point2D> randomZombieSpawns= new ArrayList<Point2D>();
+  ArrayList<Point2D> randomZombieSpawnsIJ = new ArrayList<Point2D>();
+  
   ArrayList<Point2D> lineZombieSpawns= new ArrayList<Point2D>();
+  ArrayList<Point2D> lineZombieSpawnsIJ= new ArrayList<Point2D>();
   
   double compsize = GraphicsComponent.COMPSIZE;
   
@@ -35,7 +40,7 @@ public class LevelGraphics extends Group
 	this.mapHeight=mapHeight;
 	this.mapCenterX=mapWidth/2;
 	this.mapCenterY=mapHeight/2;
-	this.level=level;
+	this.level=level - 1;
 	this.initialize();
 	
   }
@@ -65,9 +70,11 @@ public class LevelGraphics extends Group
 		  {
 		  
 		  case 0: 
+			if(tilecount==5)
+			{tilecount=1;}
 			  
-		    Floor tmp = new Floor(i,j);
-		    Floor tmp2= new Floor(i,j);
+		    Floor tmp = new Floor(i,j,tilecount);
+		    Floor tmp2= new Floor(i,j,tilecount);
 		 
 		    tmp.setSceneLocation(mapCenterX,mapCenterY);
 		    tmp2.setSceneLocation(mapCenterX, mapCenterY);
@@ -82,13 +89,17 @@ public class LevelGraphics extends Group
 		  
 		    floor.add(tmp);
 		    ceiling.add(tmp2);
+		    tilecount++;
 		    break;
 		    
 		  
 		  
 		  case 2: 
-			    Floor roomtmp = new Floor(i,j);
-			    Floor roomtmp2= new Floor(i,j);
+			  if(tilecount==5)
+				{tilecount=1;}
+			  
+			    Floor roomtmp = new Floor(i,j,tilecount);
+			    Floor roomtmp2= new Floor(i,j,tilecount);
 			 
 			    roomtmp.setSceneLocation(mapCenterX,mapCenterY);
 			    roomtmp2.setSceneLocation(mapCenterX, mapCenterY);
@@ -100,13 +111,23 @@ public class LevelGraphics extends Group
 			    roomtmp2.setRotationAxis(Rotate.X_AXIS);
 			    roomtmp2.setRotate(90);
 			    roomtmp2.setTranslateY(-GraphicsComponent.COMPSIZE/2);
-			    //add fire trap bullshit
+			    GraphicsFireTrap fireTmp = new GraphicsFireTrap(i,j);
+			    
+			    fireTmp.setSceneLocation(mapCenterX, mapCenterY);
+			    fireTmp.setTranslateY(compsize/4);
+			    
+			    fireTraps.add(fireTmp);
 			    floor.add(roomtmp);
 			    ceiling.add(roomtmp2);
+			    tilecount++;
 			    break;
+		 
 		  case 3: 
-			    Floor randomZombiespawntmp = new Floor(i,j);
-			    Floor randomZombiespawntmp2= new Floor(i,j);
+			  if(tilecount==5)
+				{tilecount=1;}
+			  
+			    Floor randomZombiespawntmp = new Floor(i,j,tilecount);
+			    Floor randomZombiespawntmp2= new Floor(i,j,tilecount);
 			 
 			    randomZombiespawntmp.setSceneLocation(mapCenterX,mapCenterY);
 			    randomZombiespawntmp2.setSceneLocation(mapCenterX, mapCenterY);
@@ -118,9 +139,13 @@ public class LevelGraphics extends Group
 			    randomZombiespawntmp2.setRotationAxis(Rotate.X_AXIS);
 			    randomZombiespawntmp2.setRotate(90);
 			    randomZombiespawntmp2.setTranslateY(-GraphicsComponent.COMPSIZE/2);
+			    floor.add(randomZombiespawntmp);
+			    ceiling.add(randomZombiespawntmp2);
+			    
 			    Point2D randomzombiePosition = new Point2D(randomZombiespawntmp.renderSpaceX,randomZombiespawntmp.renderSpaceZ);
 			    randomZombieSpawns.add(randomzombiePosition);
 			    
+			    tilecount++;
 			    break;
 		  
 		  case 4: 
@@ -129,13 +154,16 @@ public class LevelGraphics extends Group
 		  	      walls.add(walltmp); 
 		  	      break;
 		  
-		  case 5: exit = new Door(i,j); 
-		          exit.setSceneLocation(mapCenterX, mapCenterY);
+		  case 5: Door exitTmp = new Door(i,j); 
+		          exitTmp.setSceneLocation(mapCenterX, mapCenterY);
+		          exit.add(exitTmp);
 		          break;
 		  
 		  case 6:
-			    Floor lineZombiespawntmp = new Floor(i,j);
-			    Floor lineZombiespawntmp2= new Floor(i,j);
+			  if(tilecount==5)
+				{tilecount=1;}
+			    Floor lineZombiespawntmp = new Floor(i,j,tilecount);
+			    Floor lineZombiespawntmp2= new Floor(i,j,tilecount);
 			 
 			    lineZombiespawntmp.setSceneLocation(mapCenterX,mapCenterY);
 			    lineZombiespawntmp2.setSceneLocation(mapCenterX, mapCenterY);
@@ -147,13 +175,23 @@ public class LevelGraphics extends Group
 			    lineZombiespawntmp2.setRotationAxis(Rotate.X_AXIS);
 			    lineZombiespawntmp2.setRotate(90);
 			    lineZombiespawntmp2.setTranslateY(-GraphicsComponent.COMPSIZE/2);
+			    
+			    floor.add(lineZombiespawntmp);
+			    ceiling.add(lineZombiespawntmp2);
+			    
 			    Point2D linezombiePosition = new Point2D(lineZombiespawntmp.renderSpaceX,lineZombiespawntmp.renderSpaceZ);
+			    Point2D linezombieIJ= new Point2D(i ,j);
+			    
 			    lineZombieSpawns.add(linezombiePosition);
+			    lineZombieSpawnsIJ.add(linezombieIJ);
+			    tilecount++;
 			  break;
 		  
 		  case 7:
-			    Floor mastertmp = new Floor(i,j);
-			    Floor mastertmp2= new Floor(i,j);
+			  if(tilecount==5)
+				{tilecount=1;}
+			    Floor mastertmp = new Floor(i,j,tilecount);
+			    Floor mastertmp2= new Floor(i,j,tilecount);
 			 
 			    mastertmp.setSceneLocation(mapCenterX,mapCenterY);
 			    mastertmp2.setSceneLocation(mapCenterX, mapCenterY);
@@ -171,12 +209,15 @@ public class LevelGraphics extends Group
 			    
 			    masterZombieX=mastertmp.renderSpaceX;
 			    masterZombieZ= mastertmp.renderSpaceZ;
+			    tilecount++;
 			    
 			    break;
 		  
 		  case 8: 
-			  	Floor playertmp = new Floor(i,j);
-			    Floor playertmp2= new Floor(i,j);
+			  if(tilecount==5)
+				{tilecount=1;}
+			  	Floor playertmp = new Floor(i,j,tilecount);
+			    Floor playertmp2= new Floor(i,j,tilecount);
 			 
 			    playertmp.setSceneLocation(mapCenterX,mapCenterY);
 			    playertmp2.setSceneLocation(mapCenterX, mapCenterY);
@@ -195,15 +236,19 @@ public class LevelGraphics extends Group
 			    playerStartX=playertmp.renderSpaceX;
 			    playerStartZ= playertmp.renderSpaceZ;
 			    
+			    tilecount++;
 			    break;
 		  
 		  case 9:
-			  
-			  Floor obstacleSpawntmp = new Floor(i,j);
-		      Floor obstacleSpawntmp2= new Floor(i,j);
+			  if(tilecount==5)
+				{tilecount=1;}
+			  Floor obstacleSpawntmp = new Floor(i,j,tilecount);
+		      Floor obstacleSpawntmp2= new Floor(i,j,tilecount);
+		      
 		      Obstacle ob = new Obstacle(i,j);
 		      
 		      ob.setSceneLocation(mapCenterX, mapCenterY);
+		      ob.setTranslateY(compsize/4);
 		      obstacleSpawntmp.setSceneLocation(mapCenterX,mapCenterY);
 		      obstacleSpawntmp2.setSceneLocation(mapCenterX, mapCenterY);
 		  
@@ -219,7 +264,7 @@ public class LevelGraphics extends Group
 		      floor.add(obstacleSpawntmp);
 		      ceiling.add(obstacleSpawntmp2);
 		      obstacles.add(ob);
-		      
+		      tilecount++;
 		      break;
 		  }
 		  
@@ -232,8 +277,9 @@ public class LevelGraphics extends Group
 	this.getChildren().addAll(walls);
 	this.getChildren().addAll(ceiling);
 	this.getChildren().addAll(obstacles);
-	this.getChildren().add(exit);
-  
+	this.getChildren().addAll(fireTraps);
+	this.getChildren().addAll(exit);
+	
   
   }
 }
