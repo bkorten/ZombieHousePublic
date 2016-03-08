63131105057b1e97adc475cd038e5065feb0aea6
bkorten <bkorten@unm.edu>
2016-03-08 11:58:26 -0700

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,24 +1,21 @@
+import java.util.ArrayList;
+
 import javafx.event.EventHandler;
+import javafx.scene.Cursor;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.transform.Rotate;
 
 
 public class Controller 
 {
-	private static final double CAMERA_INITIAL_DISTANCE = -1500;
-	private static final double CAMERA_INITIAL_X_ANGLE = 70.0;
-	private static final double CAMERA_INITIAL_Y_ANGLE = 320.0;
-	private static final double CAMERA_NEAR_CLIP = 0.1;
-	private static final double CAMERA_FAR_CLIP = 10000.0;
-	private static final double CONTROL_MULTIPLIER = 0.1;
-    private static final double SHIFT_MULTIPLIER = 10.0;
-    private static final double MOUSE_SPEED = 0.1;
-    private static final double ROTATION_SPEED = 2.0;
-    private static final double TRACK_SPEED = 0.3;
     Player player;
-    private int change = 10;
+    ArrayList<Wall> walls;
+    private int change = 50;
+   
+    Rotate yRotate= new Rotate(0,0,0,0,Rotate.Y_AXIS);
     double mousePosX;
     double mousePosY;
     double mouseOldX;
@@ -26,17 +23,31 @@ public class Controller
     double mouseDeltaX;
     double mouseDeltaY;
     double transX, transZ;
+    double sightX,sightZ;
+    
+    GameState gameState;
+    
     
     public Controller(Player player)
     {
     	this.player=player;
+    	this.player.playerCamera.getTransforms().add(yRotate);
+    	this.player.playerLight.getTransforms().add(yRotate);
+    }
+    
+    public void setWalls(ArrayList<Wall> walls)
+    {
+    	this.walls=walls;
     }
     
 	public void handleMouse(Scene scene, final Node root) {
+		
+		 
         scene.setOnMouseMoved(new EventHandler<MouseEvent>() {
             @Override public void handle(MouseEvent me) {
                 mousePosX = me.getSceneX();
                 mousePosY = me.getSceneY();
+                
                 mouseOldX = me.getSceneX();
                 mouseOldY = me.getSceneY();
             }
@@ -47,57 +58,117 @@ public class Controller
                 mouseOldY = mousePosY;
                 mousePosX = me.getSceneX();
                 mousePosY = me.getSceneY();
+               
+                
+                
                 mouseDeltaX = (mousePosX - mouseOldX); 
-                
-               double yawRotate = player.yRotate.getAngle() + (mouseDeltaX/5);
-               player.yRotate.setAngle(yawRotate);
                
-               
-                
+               double yawRotate = (yRotate.getAngle() + (mouseDeltaX*1.01));
+        
+               //player.translatePlayer(10, 10);
+               yRotate.setAngle(yawRotate);
+                      
+             //  System.out.println("Rotate angle "+ yRotate.getAngle());
+              
             }
         });
     }
 	
 	public void handleKeyboard(Scene scene, final Node root) {
         scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-          double rotateAngle = player.yRotate.getAngle();
          
+         
+          
+          private boolean isCollision()
+          {
+        	  
+        	  double distance;
+        	  for(Wall wall : walls)
+        	  {
+        		  
+        		  distance = 
+        				 
+        						  Math.pow( ((player.playerCurX + transX) - wall.renderSpaceX),2) 
+        				  + 
+        				  		  Math.pow(((player.playerCurZ +transZ)- wall.renderSpaceZ), 2);
+        	     
+        		  
+        		  if(distance  <  Math.pow(100,2))
+        		  {
+        			System.out.println("Collision at "+wall.renderSpaceX+" "+wall.renderSpaceZ);  
+        		    return true;
+        		  }
+        	  }
+        	  return false;
+        	  
+          }
+          
           private void forward()
           {
-        	double ra = (rotateAngle/180 * 3.14);    
-        	transX = Math.cos(ra) * 2;
-        	transZ = Math.sin(ra) * 2;
-        	
-            player.translatePlayer(transX, transZ);
+        	 
+        	  double rotateAngle = yRotate.getAngle();
+        	     
+        	  double ra = Math.toRadians(rotateAngle);   
         	  
+        	    
+        	  transX =   Math.sin(ra) * change ;
+              
+              transZ =   Math.cos(ra) * change ;
+        	  
+             //if(!isCollision())
+              
+             player.translatePlayer(transX, transZ);
+             
+            
+             
           }
           
           private void backward()
           {
-        	   transZ= -change;
-               player.translatePlayer(transX, transZ);
+        	  double rotateAngle = yRotate.getAngle();
+        	  double ra = Math.toRadians(rotateAngle);    
+              
+        	  transX =   Math.sin(ra) * -change;
+              transZ =   Math.cos(ra) * -change;
+        	
+             // if(!isCollision()) 
+             player.translatePlayer(transX, transZ);
+           	
           }
           
           private void left()
           {
-        	double ra = (rotateAngle/180 * 3.14);    
-          	transX = Math.cos(ra) * 10;
-          	transZ = Math.sin(ra) * 10;
-            player.translatePlayer(transX, transZ);
+        	  double rotateAngle = yRotate.getAngle();
+        	double ra = Math.toRadians(rotateAngle-90);    
+          	transX =   Math.sin(ra) * change;
+          	transZ =   Math.cos(ra) * change;
+          	
+          	   // if(!isCollision())
+          		player.translatePlayer(transX, transZ);
+        	 
           }
           
           private void right()
           {
-        	double ra = (rotateAngle/180 * 3.14);    
-          	transX = Math.cos(ra) * -10;
-          	transZ = Math.sin(ra) * -10;
-            player.translatePlayer(transX, transZ);
+        	  double rotateAngle = yRotate.getAngle();
+        	   double ra = Math.toRadians(rotateAngle-90);    
+               transX =   Math.sin(ra) * change;
+               transZ =   Math.cos(ra) * change;
+          	
+          	
+               //if(!isCollision()) 
+          		player.translatePlayer(-transX, -transZ);
+        	 
           
           }
         	@Override
-            public void handle(KeyEvent event) {
+            public void handle(KeyEvent event) 
+        	{
              
-                
+        		if(event.isShiftDown())
+        			change = 75;
+        		else
+        			change = 20; 
             	switch (event.getCode()) 
                 {
                 case W: forward();break;
@@ -109,8 +180,10 @@ public class Controller
                 case D: right(); break;
 
                 }
-                   
-            }
+            	
+            	
+            	
+        	}
         });
     }
 }
