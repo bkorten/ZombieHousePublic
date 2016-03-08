f3d24f3eb88b8f5c0072d819f7579703ebe31604
jamperr <jp55674@unm.edu>
2016-03-08 12:45:38 -0700

    finalized ZombieSoundWorker

--- ZombieSoundWorker.java
+++ ZombieSoundWorker.java
@@ -1,23 +1,37 @@
-package resources.sound;
-import java.net.URI;
+/**
+ * This class is used to hold a single sound file and play it 
+ * whenever GameState sets the play boolean equal to true.
+ */
 import java.net.URL;
 import javafx.scene.media.AudioClip;
-import javafx.scene.media.MediaPlayer;
-
-
 public class ZombieSoundWorker extends Thread
 {
-  
+  //holds the sound
   final AudioClip sound;
+  //terminate the thread
   private boolean terminate;
-  private double volume;
-  private double balance;
-  private double rate; 
-  private double pan;
   public void setTerminate(boolean b){this.terminate = b;}
+  
+  private double volume;
+  public void setVolume(double x){this.volume = x;}
+  
+  private double balance;
+  public void setBalance(double x){this.balance = x;}
+  
+  private double rate; 
+  public void setRate(double x){this.rate = x;}
+  
+  private double pan;
+  public void setPan(double x){this.pan = x;}
+  
   
   private boolean play;
   public void setPlay(boolean b){this.play = b;}
+  
+  /**
+   * 
+   * @param s URL to the sound file
+   */
   public ZombieSoundWorker(String s)
   {
     final URL path = this.getClass().getResource(s);
@@ -33,44 +47,30 @@ public class ZombieSoundWorker extends Thread
     {
       try 
       {
-        Thread.sleep(500);
+        Thread.sleep(100);
       } 
       catch (InterruptedException e){}
       if(play)
       {
-        
-      }
-    
-    }
-    
-    
-    
-    
+        playSound(volume,balance,rate,pan,1);
+        play = false;
+      }  
+    }           
   }
   
-
-  public void playSound() 
-  {
-    sound.play();
-    while(sound.isPlaying()){}
-  }
-  
+  /**
+   * Plays the sound
+   * @param volume Volume of the sound from 0.0 to 1.0
+   * @param balance Balance of the sound from -1.0 to 1.0
+   * @param rate How fast to play the sound, 1.0 is default
+   * @param pan "Center" of sound from -1.0 to 1.0 
+   */
   public void playSound(double volume, double balance, double rate, 
                         double pan, int priority)
   {
-    sound.play(volume, balance, rate, rate, priority);
-    while(sound.isPlaying()){}
+    System.out.println(balance);
+    sound.play(volume, balance, rate, rate, 1);
+    while(sound.isPlaying())
+    {}
   }
-  
-  
-  public static void main(String[] args) 
-  {
-    ZombieSoundWorker s = new ZombieSoundWorker("FootStep.wav");
-    ZombieSoundWorker t = new ZombieSoundWorker("Growl_01.wav");
-    ZombieSoundWorker u = new ZombieSoundWorker("Growl_02.wav");
-    ZombieSoundWorker v = new ZombieSoundWorker("Growl_03.wav");
-    ZombieSoundWorker w = new ZombieSoundWorker("Growl_04.wav");
-  }
-
-
 }
