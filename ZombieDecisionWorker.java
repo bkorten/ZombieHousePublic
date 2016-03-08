87a28ba8fc1419ec09863445e835231b11204650
jamperr <jp55674@unm.edu>
2016-03-08 07:22:33 -0700

    finalized zombieDecisionWorker

--- ZombieDecisionWorker.java
+++ ZombieDecisionWorker.java
@@ -1,19 +1,29 @@
-
+/*
+ * CS 351: Project-Zombie House
+ * James Perry
+ * This class is used to make decisions for all of the
+ * different zombies every zombieDecisionRate seconds.
+ */
 public class ZombieDecisionWorker extends Thread
 {
   private boolean isGameRunning = false;
   public void setRunning(boolean b){isGameRunning = b;}
   
+  //should this thread terminate itself
   private boolean terminate = false;
   public void setTerminate(boolean b){this.terminate = b;}
   
   private double zombieDecisionRate;
   public void setZombieDecisionRate(double x){this.zombieDecisionRate = x;}
+  
   private GameState game;
+  /*
+  *@param GameState game The GameState object from which we will grab the decision rate.
+  */
   public ZombieDecisionWorker(GameState game)
   {
     this.game = game;
-    this.zombieDecisionRate = game.getZombieDecisionRate();
+    this.zombieDecisionRate = game.getZombieDecisionRate()*1000;
   }
   
   @Override
@@ -23,14 +33,16 @@ public class ZombieDecisionWorker extends Thread
     {
       while(isGameRunning)
       {
-        game.makeZombieDecisions();
+        game.makeRandomZombieDecisions();
+        game.makeLineZombieDecisions();
+        game.makeMasterZombieDecision();
         try
         {
           sleep((long) zombieDecisionRate);
         }
-        catch(InterruptedException e){}
+        catch(InterruptedException e)
+        {}
       }
-      
       try
       {
         sleep(500);
